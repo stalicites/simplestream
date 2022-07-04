5d46e5a757d9b58cfa6c5af36b0fe5c20c035752
Stalicites <88872425+stalicites@users.noreply.github.com>
2022-07-04 15:23:32 -0700

    Update stream.js

--- room/stream.js
+++ room/stream.js
@@ -16,7 +16,8 @@ socket.setDefaultConfig({ publishToSelf: false });
 function encode(c) { var x = 'charCodeAt', b, e = {}, f = c.split(""), d = [], a = f[0], g = 256; for (b = 1; b < f.length; b++)c = f[b], null != e[a + c] ? a += c : (d.push(1 < a.length ? e[a] : a[x](0)), e[a + c] = g, g++, a = c); d.push(1 < a.length ? e[a] : a[x](0)); for (b = 0; b < d.length; b++)d[b] = String.fromCharCode(d[b]); return d.join("") }
 function decode(b) { var a, e = {}, d = b.split(""), c = f = d[0], g = [c], h = o = 256; for (b = 1; b < d.length; b++)a = d[b].charCodeAt(0), a = h > a ? d[b] : e[a] ? e[a] : f + c, g.push(a), c = a.charAt(0), e[o] = f + c, o++, f = a; return g.join("") }
 
-const startStreamButton = document.getElementById("startStream");
+const startStreamCameraButton = document.getElementById("startStreamCamera");
+const startStreamScreenButton = document.getElementById("startStreamScreen");
 const output = document.getElementById("output");
 const data = document.getElementById("data");
 const stream = document.getElementById("stream");
@@ -29,30 +30,44 @@ document.title = waiting;
 const color = '#' + Math.floor(Math.random() * 16777215).toString(16);
 
 //Claim Room
-startStreamButton.addEventListener('click', async function () {
-  let camOutput = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
-  output.srcObject = camOutput;
+let streamOutput;
+
+startStreamCameraButton.addEventListener('click', async function () {
+  streamOutput = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
+  startStream();
+});
+startStreamScreenButton.addEventListener('click', async function () {
+  streamOutput = await navigator.mediaDevices.getDisplayMedia({ video: { cursor: "always" }, audio: false });
+  startStream();
+});
+
+function startStream() {
+  output.srcObject = streamOutput;
   document.title = "Brodcasting Stream | " + roomCode;
   streamUsername.innerText = username;
   setInterval(function () {
-    if(!document.hidden){
+    if (!document.hidden) {
       data.getContext('2d').drawImage(output, 0, 0, data.width, data.height);
-      let frame = data.toDataURL('image/jpeg');
+      let frame = data.toDataURL('image/jpeg', 0.5);
       stream.src = frame;
 
       socket.publish({ s: roomCode, t: "f" }, { f: encode(frame), u: username });
     }
-  }, 128);
-  startStreamButton.remove();
-});
+  }, 64);
+  startStreamCameraButton.remove();
+  startStreamScreenButton.remove();
+}
 
 //Load Frame
+let lastFrame;
+
 socket.subscribe({ s: roomCode, t: "f" }, function (d) {
   if (d.f) {
     stream.src = decode(d.f);
   }
-  if (startStreamButton) {
-    startStreamButton.remove();
+  if (startStreamCameraButton) {
+    startStreamCameraButton.remove();
+    startStreamScreenButton.remove();
   }
   if (document.title == waiting) {
     document.title = "Watching Stream | " + roomCode;
@@ -60,6 +75,7 @@ socket.subscribe({ s: roomCode, t: "f" }, function (d) {
   if (streamUsername.innerText != d.u) {
     streamUsername.innerText = d.u;
   }
+  lastFrame = new Date().getTime()
 })
 
 //Chats
@@ -76,7 +92,7 @@ function createChat(message, username, color) {
 }
 
 chatInput.addEventListener("keydown", function (event) {
-  if (event.key == "Enter") {
+  if (event.key == "Enter" && chatInput.value.replace(/\s/g, "").length > 0) {
     socket.publish({ s: roomCode, t: "c" }, { u: username, c: color, m: chatInput.value });
     createChat(chatInput.value, username, color);
     chatInput.value = "";
